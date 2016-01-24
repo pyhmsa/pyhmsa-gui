e4e54c98a039197cd66506f2d60a9801cd58773a
Philippe Pinard <philippe.pinard@gmail.com>
2016-01-24 12:29:27 +0100

    More debug

--- .ci/before_install.sh
+++ .ci/before_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -f -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
@@ -11,8 +13,13 @@ conda update -q conda
 conda info -a
 conda env list
 
+# Setup variables
 testenv=testenv$TRAVIS_PYTHON_VERSION
-hasenv=`conda env list | grep $testenv`
+echo "Test environment: $testenv"
+
+hasenv=`conda env list | grep "$testenv"`
+echo "Has environment: $hasenv"
+
 requirements=`cat requirements.txt | tr '\n' ' ' | sed 's/pyhmsa//g' | sed 's/qtpy//g'`
 
 if [ -n "$hasenv" ]; then
