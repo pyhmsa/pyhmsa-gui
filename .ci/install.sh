ffdfeb8f91b9b9b2f03d501dc719eaab17c194dd
Philippe Pinard <philippe.pinard@gmail.com>
2016-01-24 13:43:03 +0100

    Improve script

--- .ci/install.sh
+++ .ci/install.sh
@@ -2,13 +2,18 @@
 
 set -e
 
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-bash miniconda.sh -b -f -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
-hash -r
-conda config --set always_yes yes --set changeps1 no
-conda update -q conda
-conda install -n root _license
+if [ ! -d "$HOME/miniconda" ]; then
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+    bash miniconda.sh -b -f -p $HOME/miniconda
+    export PATH="$HOME/miniconda/bin:$PATH"
+    hash -r
+    conda config --set always_yes yes --set changeps1 no
+    conda update -q conda
+    conda install -n root _license
+else
+    export PATH="$HOME/miniconda/bin:$PATH"
+    conda update -q conda
+fi
 
 # Echo info about conda
 conda info -a
